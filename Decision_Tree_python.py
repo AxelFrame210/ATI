b29ecf55ed18ef011b8083cfe9d6b61c3d05b89f
nhdang16 <nhda6k53@gmail.com>
2024-11-16 22:54:03 +0700

    visualize tree

--- Decision_Tree_python.py
+++ Decision_Tree_python.py
@@ -1,5 +1,4 @@
 #Python code to predict diabetes based on several factors.
-#Author: Saurav Kumar Dubey
 
 import pandas as pd
 import numpy as np
@@ -10,22 +9,34 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import precision_recall_curve
+from sklearn.tree import plot_tree
+
+# Function to plot the decision tree
+def plot_decision_tree(clf, feature_names, class_names):
+    plt.figure(figsize=(15, 8))
+    plot_tree(
+        clf,
+        feature_names=feature_names,
+        class_names=class_names,
+        filled=True,
+        fontsize=12,
+        max_depth=2
+    )
+    plt.title("Decision Tree Visualization", fontsize=16)
+    plt.show()
 
 #read data
 data = pd.read_csv("diabetes.csv")
 
 #setting up the data
-
 y=data.iloc[:,[8]]
-x=data.iloc[:,[1,2,3,4,5,6,7]]
+x=data.iloc[:,[1,2,3,4,5,6,7]] #excluded Pregnancies
 
 #preprocessing the data
-
-#le = preprocessing.LabelEncoder()
-#y=le.fit_transform(y)
+le = preprocessing.LabelEncoder()
+y=le.fit_transform(y)
 
 #splitting the data into train and test
-
 X_train,X_test,Y_train,Y_test=train_test_split(x,y,test_size=0.05,random_state=0)
 
 #training the classifier
@@ -33,12 +44,9 @@ clf=tree.DecisionTreeClassifier(criterion='gini',min_samples_split=30,splitter="
 clf=clf.fit(X_train,Y_train)
 
 #predicting
-
 y_pred=clf.predict(X_test)
 
-
 #testing the accuracy
-
 accuracy=accuracy_score(Y_test,y_pred)
 print(str(accuracy*100)+"% accuracy")
 
@@ -62,14 +70,13 @@ plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
           average_precision))
 plt.show()
 
+# Visualizing the decision tree
+plot_decision_tree(clf, feature_names=x.columns, class_names=['No Diabetes', 'Diabetes'])
 
-
-#height=pd.Series(y).value_counts(normalize=True)
-#plt.bar(range(2),height.tolist()[::-1],1/1.5,color='blue',label="classes",alpha=0.8)
-#plt.title('Decision Tree Classification')
-#plt.xlabel('1 0')
-#plt.ylabel('occurences')
-#plt.legend()
-#plt.show()
-
-
+height=pd.Series(y).value_counts(normalize=True)
+plt.bar(range(2),height.tolist()[::-1],1/1.5,color='blue',label="classes",alpha=0.8)
+plt.title('Decision Tree Classification')
+plt.xlabel('1 0')
+plt.ylabel('occurences')
+plt.legend()
+plt.show()
